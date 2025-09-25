440d74400706c3ca11142159953343089ef9480d
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-09-25 12:00:06 +0530

    Splash Screen with lotties

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      bottomNavigationBar: SafeArea(
+        child: SizedBox(
+          height: 30,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'By Vishishta Dilsara',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Lottie.asset(
+              'assets/lotties/cloudyDay.json',
+              width: 200,
+              height: 200,
+            ),
+            Text(
+              'Weather App',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
